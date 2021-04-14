5596999dfdde24db618683c6472c71a8d02a3340
Angus <pengzhixin@live.cn>
2021-04-14 20:01:29 +0800

    feat: update Enumerations

---
+++ SwiftTour.playground/Pages/Enumerations.xcplaygroundpage/Contents.swift
@@ -0,0 +1,116 @@
+// 枚举
+
+// 枚举语法
+enum CompassPoint {
+    case north
+    case south
+    case east
+    case west
+}
+
+enum Planet {
+    case mercury, venus, earth, mars, jupiter, saturn, uranus, neptune
+}
+
+var directionToHead = CompassPoint.west
+
+directionToHead = .east
+
+// 使用 Switch 语句匹配枚举值
+directionToHead = .south
+switch directionToHead {
+case .north:
+    print("Lots of planets have a north")
+case .south:
+    print("Watch out for penguins")
+case .east:
+    print("Where the sun rises")
+case .west:
+    print("Where the skies are blue")
+}
+
+// 枚举成员的遍历
+enum Beverage: CaseIterable {
+    case coffee, tea, juice
+}
+let numberOfChoices = Beverage.allCases.count
+print("\(numberOfChoices) beverages available")
+
+for beverage in Beverage.allCases {
+    print(beverage)
+}
+
+// 关联值
+enum Barcode {
+    case upc(Int, Int, Int, Int)
+    case qrCode(String)
+}
+
+var productBarcode = Barcode.upc(8, 85909, 51226, 3)
+productBarcode = .qrCode("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+switch productBarcode {
+case let .upc(numberSystem, manufacturer, product, check):
+    print("UPC: \(numberSystem), \(manufacturer), \(product), \(check)")
+case .qrCode(let productCode):
+    print("QR code: \(productCode)")
+}
+
+// 原始值
+enum ASCIIControlCharacter: Character {
+    case tab = "\t"
+    case lineFeed = "\n"
+    case carriageReturn = "\r"
+}
+
+// 原始值的隐藏值
+enum AnotherPlanet: Int {
+    case mercury = 1, venus, earth, mars, jupiter, saturn, uranus, neptune
+}
+
+enum AnotherCompassPoint: String {
+    case north, south, east, west
+}
+
+let earthsOrder = AnotherPlanet.earth.rawValue
+let sunsetDirection = AnotherCompassPoint.west.rawValue
+
+// 使用原始值初始化枚举实例
+let possiblePlanet = AnotherPlanet(rawValue: 7)
+
+let positionToFind = 11
+if let somePlanet = AnotherPlanet(rawValue: positionToFind) {
+    switch somePlanet {
+    case .earth:
+        print("Mostly harmless")
+    default:
+        print("Not a safe place for humans")
+    }
+} else {
+    print("There isn't a planet at position \(positionToFind)")
+}
+
+// 递归枚举
+indirect enum ArithmeticExpression {
+    case number(Int)
+    case addition(ArithmeticExpression, ArithmeticExpression)
+    case multiplication(ArithmeticExpression, ArithmeticExpression)
+}
+
+let five = ArithmeticExpression.number(5)
+let four = ArithmeticExpression.number(4)
+let sum = ArithmeticExpression.addition(five, four)
+let product = ArithmeticExpression.multiplication(sum, ArithmeticExpression.number(2))
+
+func evaluate(_ expression: ArithmeticExpression) -> Int {
+    switch expression {
+    case let .number(value):
+        return value
+    case let .addition(left, right):
+        return evaluate(left) + evaluate(right)
+    case let .multiplication(left, right):
+        return evaluate(left) * evaluate(right)
+    }
+}
+
+print(evaluate(product))
